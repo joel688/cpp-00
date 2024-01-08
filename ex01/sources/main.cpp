b451e35d11f6af859c76578213ec1df264ef87ff
Joachim Koenig <joachimkoenig@MacBook-Air-de-Joachim.local>
2024-01-09 00:21:21 +0100

    push

--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -18,21 +18,21 @@ int main (void)
   std::string  command;
   PB.num = 0;
 
-  while(command != "4")
+  while(command != "EXIT")
   {
-		std::cout << "Please type : 1 to add contact, 2 to search contact, 3 to show all contact, and 4 to exit" << std::endl;
+		std::cout << "Please type : ADD to add contact, SEARCH to search contact, SHALL to show all contact, and EXIT to exit" << std::endl;
     std::cin >>  command;
 
-    if (command == "1")
+    if (command == "ADD")
     {
       while(PB.num <= 8 && PB.nb_contact[PB.num].name != "NULL")
         PB.num++;
       PB.ft_add_contact();
       PB.num = 0;
     }
-    if (command == "2")
+    if (command == "SEARCH")
       PB.ft_search_contact();
-    if (command == "3")
+    if (command == "SHALL")
       PB.ft_show_all();
   }
 }
